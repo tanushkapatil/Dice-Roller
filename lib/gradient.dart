3f1cf2db9df1ad6257b0e7689708445091333657
Tanushka Patil <tanushkapatil2412@gmail.com>
2025-01-21 21:36:30 +0530

    changes

--- lib/gradient.dart
+++ lib/gradient.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/dice_roller.dart';
 
 var startAlignment = Alignment.topLeft;
 var endAlignment = Alignment.bottomRight;
 
-class GradiantContainer extends StatelessWidget {
-  const GradiantContainer(this.color1, this.color2, {super.key});
-  final color1;
-  final color2;
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
 
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
@@ -18,8 +20,8 @@ class GradiantContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Image.asset('assets/images/dice-2.png', width: 200),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradiantContainer(
+        body: GradientContainer(
           Color.fromARGB(255, 33, 5, 156),
           Color.fromARGB(255, 113, 37, 212),
         ),
