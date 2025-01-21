8dc1aaf17d73e566ddf501c70b2ebec4cfb974a5
Tanushka Patil <tanushkapatil2412@gmail.com>
2025-01-21 17:18:00 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+  runApp(
+    MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('First App'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text(
-                'Hello World!',
-              ),
-              Text(
-                'It\'s time to learn Flutter!',
-              ),
-            ],
-          ),
-        ),
+        body: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+                Color.fromARGB(255, 132, 0, 255),
+                Color.fromARGB(255, 83, 0, 160),
+              ]),
+            ),
+            child: const Center(
+              child: Text("Hello World"),
+            )),
       ),
-    );
-  }
+    ),
+  );
 }
