6f08c41059bf75dd986725e149cb526a2df174f7
Tanushka Patil <tanushkapatil2412@gmail.com>
2025-01-21 21:44:48 +0530

    changes

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 class DiceRoller extends StatefulWidget {
@@ -10,10 +11,12 @@ class DiceRoller extends StatefulWidget {
 }
 
 class _DiceRollerState extends State<DiceRoller> {
-  var activeDiceImage = 'assets/images/dice-2.png';
+  var currentDiceRoll = 2;
 
   void rollDice() {
-    activeDiceImage = 'assets/images/dice-4.png';
+    setState(() {
+      currentDiceRoll = Random().nextInt(6) + 1;
+    });
   }
 
   @override
@@ -22,7 +25,7 @@ class _DiceRollerState extends State<DiceRoller> {
       mainAxisSize: MainAxisSize.min,
       children: [
         Image.asset(
-          activeDiceImage,
+          'assets/images/dice-$currentDiceRoll.png',
           width: 200,
         ),
         const SizedBox(height: 20),
